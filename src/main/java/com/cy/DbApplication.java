1cd53424ede854c8384f33faec59600f54bc13ac
王辉 <wangh_0929@163.com>
2020-06-26 19:23:53 +0800

    初始化项目

---
+++ src/main/java/com/cy/DbApplication.java
@@ -0,0 +1,17 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wh
+ * @date 2020/5/26
+ * Description: 程序入口
+ * 访问地址：http://localhost/doIndexUI
+ */
+@SpringBootApplication
+public class DbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbApplication.class, args);
+    }
+}
